02854d9ef1ddd1032ef087ed746c3c253ec05fd1
lubnaa <lubnaamuhammed@gmail.com>
2025-06-02 02:22:08 +0300

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,6 @@
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript";
+document.querySelector("body").appendChild(h2);
+const h3 = document.createElement("h3");
+h3.textContent = "by the one and only Lubnaa Simalo Mohamed <3"
+document.querySelector("body").appendChild(h3);
